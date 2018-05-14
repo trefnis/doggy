5e822792999d170fa1948fc23c90a0edc1e6e82f
Michał Olchawa <michal.olchawa@netguru.co>
2018-05-15 01:14:26 +0200

    Upgrade `material-ui` package.

--- src/components/PhotoTile.js
+++ src/components/PhotoTile.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import Card, { CardHeader, CardContent, CardMedia } from 'material-ui/Card';
-import Typography from 'material-ui/Typography';
-import Avatar from 'material-ui/Avatar';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
 
 import { media } from '../utils/rwd';
 import { getCorrectImageProps } from '../utils/tileSizing';
